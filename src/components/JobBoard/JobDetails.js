d0c48a42f27b6d58f6056f3ab34be2ebea82bdec
James Skarzinskas <james@jskarzin.org>
2022-02-13 11:50:04 -0500

    feat(jobs): Add dummied archive action to JobDetails

--- src/components/JobBoard/JobDetails.js
+++ src/components/JobBoard/JobDetails.js
@@ -1,4 +1,7 @@
-import { PencilIcon } from "@heroicons/react/solid";
+import {
+    PencilIcon,
+    ArchiveIcon
+} from "@heroicons/react/solid";
 
 const JobDetails = ({ row, editJob }) => {
     return (
@@ -7,7 +10,7 @@ const JobDetails = ({ row, editJob }) => {
                 Here we will display extended information / contextual actions for the job.
             </div>
 
-            <div>
+            <div className="mt-2">
                 <button
                     type="button"
                     onClick={() => editJob(row.original)}
@@ -15,6 +18,15 @@ const JobDetails = ({ row, editJob }) => {
                 >
                     <PencilIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
                     Edit
+                </button>
+
+                <button
+                    type="button"
+                    onClick={() => false}
+                    className="inline-flex items-center ml-2 px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    <ArchiveIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                    Archive Job
                 </button>
             </div>
         </div>
