05e4ad3aa2a9589a026193b7c9512d7dede462e7
James Skarzinskas <james@jskarzin.org>
2022-02-13 19:35:17 -0500

    chore: tweak mock values in advance of morning demo

--- src/util/mock.js
+++ src/util/mock.js
@@ -9,9 +9,9 @@ export const MOCK_PROJECT_DATA = [
 
 export const MOCK_JOB_DATA = [
     { id: 1, name: 'Legacy Web Project', budget_hours: 50, budget: 7000.00, project_status: 'In Development', project_ids: [1, 2], resource_ids: [2], allocations: {2: 0.7} },
-    { id: 2, name: 'Big SaaS Project', budget_hours: 800, budget: 270000.00, project_status: 'In Development', project_ids: [3], resource_ids: [2, 3], allocations: {2: 0.3, 3: 1.0} },
+    { id: 2, name: 'Big SaaS Project', budget_hours: 800, budget: 270000.00, project_status: 'In Development', project_ids: [3], resource_ids: [2, 3], allocations: {2: 0.3, 3: 0.9} },
     { id: 3, name: 'Just Make It Work!', budget_hours: 30, budget: 20.00, project_status: 'In Development', project_ids: [4], resource_ids: [1], allocations: {1: 0.7} },
-    { id: 4, name: 'Thing-o-matic Integration', budget_hours: 70, budget: 5000.00, project_status: 'Pending', project_ids: [5], resource_ids: [], allocations: {} },
+    { id: 4, name: 'Thing-o-matic Integration', budget_hours: 70, budget: 5000.00, project_status: 'Pending', project_ids: [5], resource_ids: [3], allocations: {3: 0.1} },
     { id: 5, name: 'Other Test', budget_hours: 10, budget: 1000.00, project_status: 'In Development', project_ids: [6], resource_ids: [1], allocations: {1: 0.3} }
 ];
 
